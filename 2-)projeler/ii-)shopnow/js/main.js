4f374a30a6fbddac9f179bba125cc98b8c60bd47
yusufyaman07 <yamanyusuf00000@gmail.com>
2025-05-17 19:37:43 +0300

    shopNow product,contact ve footer kısımları || shopNow bitiş

--- 2-)projeler/ii-)shopnow/js/main.js
+++ 2-)projeler/ii-)shopnow/js/main.js
@@ -1,6 +1,11 @@
-import { uiElement } from "./ui.js";
+import products from "./products.js";
+import { renderProducts, uiElement } from "./ui.js";
 
 // Menu butonuna  tıklanınca çalışacak fonksiyon
 uiElement.mobileMenuBtn.addEventListener("click", () => {
   uiElement.nav.classList.toggle("active");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderProducts(products);
 });
--- 2-)projeler/ii-)shopnow/js/ui.js
+++ 2-)projeler/ii-)shopnow/js/ui.js
@@ -1,6 +1,38 @@
+// UI Element
 const uiElement = {
   mobileMenuBtn: document.querySelector(".mobile-menu-btn"),
   nav: document.querySelector("nav"),
+  productWrapper: document.querySelector(".product-wrapper"),
 };
 
-export { uiElement };
+// UI Functions
+
+const renderProducts = (products) => {
+  console.log(products);
+
+  // Dışarıdan verilen products dizisini dön ve bu dizinin her elemanı için bir html oluştur
+  products.forEach((product) => {
+    // Her ürün için bir div oluştur
+    const productCard = document.createElement("div");
+    // Oluşturulan ürün kartına product-card classı ekle
+    productCard.classList.add("product-card");
+
+    // Oluşturulan,class atanan elemanın html'ini belirle
+    productCard.innerHTML = `            <img
+              src="${product.image}"
+              alt="product-card-image"
+            />
+
+            <div class="info">
+              <h2>${product.name}</h2>
+              <h4>${product.category}</h4>
+              <p>₺${product.price}</p>
+              <button class="btn">Sepete Ekle</button>
+            </div>`;
+
+    // Oluşturulan,classı eklenen ve html'i belirlenen elamanı arayüzdeki productWrapper içerisine ekle
+    uiElement.productWrapper.appendChild(productCard);
+  });
+};
+
+export { uiElement, renderProducts };
